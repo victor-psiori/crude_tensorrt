4cf206e9105461e220da88a7620ae5d67ef1342d
victor <victor@psiori.com>
2020-09-18 20:13:02 +0200

    added logger for inference

--- inference_in_trt.cpp
+++ inference_in_trt.cpp
@@ -3,6 +3,21 @@
 
 using namespace std;
 
+class Logger : public nvinfer1::ILogger {
+public:
+	nvinfer1::ILogger& getTRTLogger() {
+		return *this;
+	}
+
+	void log(Severity severity, const char* msg) override {
+		//suppress info level messages
+		if (severity != Severity::kINFO) {
+			std::cout << msg << std::endl;
+		}
+	}
+
+};
+
 int main () {
 	cout << "Hey hey" << endl;
 }
