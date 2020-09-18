42e1f66d2195c5b4bc07c19d04684b3f36513eed
victor <victor@psiori.com>
2020-09-18 22:04:27 +0200

    ctor and deserialization of engine file is done

--- inference_in_trt.cpp
+++ inference_in_trt.cpp
@@ -1,23 +1,56 @@
-#include <NvInfer.h>
-#include <iostream>
+#include "inference_in_trt.hpp"
+#include <cassert>
 
 using namespace std;
 
-class Logger : public nvinfer1::ILogger {
-public:
-	nvinfer1::ILogger& getTRTLogger() {
-		return *this;
-	}
+namespace autocrane {
 
-	void log(Severity severity, const char* msg) override {
-		//suppress info level messages
-		if (severity != Severity::kINFO) {
-			std::cout << msg << std::endl;
-		}
-	}
 
-};
+TrtObjectDetector::TrtObjectDetector(const std::string filename) :
+	engineFile(filename) {
+		std::ifstream file(engineFile, std::ios::binary);
+		vector<char> trtModelStreamFromFile;
+		extractContentsToBuffer(file, trtModelStreamFromFile);
+		Logger gLogger;
+		// for plugin deserialization errors.
+		// nvinfer1::initLibNvInferPlugins(&gLogger, "");
+		// nvinfer1::IRuntime* runtime_ = nvinfer1::createInferRuntime(gLogger.getTRTLogger());
+		// std::unique_ptr<IRuntime, Destroy> runtime{createInferRuntime(gLogger)};
+
+
+		TRTUniquePtr<nvinfer1::IRuntime> runtime{nvinfer1::createInferRuntime(gLogger)};
+		assert(runtime != nullptr);
+
+		// mEngine = runtime->nvinfer1::IRuntime::deserializeCudaEngine(
+		// 	trtModelStreamFromFile.data(), size_engine);
+
+		
+		mEngine = TRTUniquePtr<nvinfer1::ICudaEngine>(
+			runtime->deserializeCudaEngine(trtModelStreamFromFile.data(), size_engine));
+}
+
+void TrtObjectDetector::extractContentsToBuffer(std::ifstream& file,
+																							   std::vector <char>& buf) {
+	if (file.good()) {
+		file.seekg(0, file.end);
+		size_engine = file.tellg();
+		file.seekg(0, file.beg);
+		buf.resize(size_engine);
+		std::cout << "size of engine file: " << buf.size() << std::endl;
+		file.read(buf.data(), size_engine);
+		file.close();
+	}
+}
+
+TrtObjectDetector::~TrtObjectDetector() {
+}
+
+
+
+}	//namespace autocrane
+
 
 int main () {
 	cout << "Hey hey" << endl;
 }
+
---
+++ inference_in_trt.hpp
@@ -0,0 +1,65 @@
+#ifndef AUTOCRANE_TRT_OBJDETECTOR
+#define AUTOCRANE_TRT_OBJDETECTOR
+
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <memory>
+#include <NvInfer.h>
+#include <NvInferPlugin.h>
+
+namespace autocrane {
+
+class Logger : public nvinfer1::ILogger {
+public:
+	nvinfer1::ILogger& getTRTLogger() {
+		return *this;
+	}
+
+	void log(Severity severity, const char* msg) override {
+		//suppress info level messages
+		if (severity != Severity::kINFO) {
+			std::cout << msg << std::endl;
+		}
+	}
+
+};
+
+//destroy TensorRT objects if something goes wrong
+struct TRTDestroy{
+	template <typename T>
+	void operator()(T* obj) const {
+		if (obj) {
+			obj->destroy();
+		}
+	}
+};
+
+template<typename T>
+using TRTUniquePtr = std::unique_ptr<T, TRTDestroy>;
+
+class TrtObjectDetector {
+public:
+	TrtObjectDetector(const std::string filename);
+	virtual ~TrtObjectDetector();
+	/**
+	 * @brief function extracts content in .bin engine file to char vector
+	 * @param file: ifstream object to which .bin engineFile is loaded in bin mode
+	 * @param buf: char buffer to extracts contents of file
+	*/
+	void extractContentsToBuffer(std::ifstream& file, std::vector <char>& buf);
+
+
+private:
+	std::string engineFile;
+	std::size_t size_engine;
+	// nvinfer1::ICudaEngine* mEngine;
+	TRTUniquePtr<nvinfer1::ICudaEngine> mEngine;
+};
+
+}; // namespace autocrane
+
+
+
+#endif /* AUTOCRANE_TRT_OBJDETECTOR */
